82c9b27ff236173091ada5460995a828485ab31a
Vasics Enikő <vasics.eniko@edu.bme.hu>
2024-11-14 15:32:13 +0100

    jobban jelenik meg

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,23 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args){
         JFrame window= new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(true);
+        window.setResizable(false);
         window.setTitle("Labirinth Extreme");
 
         GamePanel gamePanel= new GamePanel();
         window.add(gamePanel);
+        window.setSize(gamePanel.screenWidth,gamePanel.screenHight);
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
+        gamePanel.setUpGame();
         gamePanel.startGameThread();
 
     }
